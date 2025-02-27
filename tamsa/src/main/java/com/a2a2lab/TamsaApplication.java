971479e7a0da6f8c66604dcec2bda7fa5cdcd8b3
tdj4909 <tdj4909@gmail.com>
2025-02-27 12:33:03 +0900

    update

---
+++ tamsa/src/main/java/com/a2a2lab/TamsaApplication.java
@@ -0,0 +1,13 @@
+package com.a2a2lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamsaApplication.class, args);
+	}
+
+}
---
+++ tamsa/src/test/java/com/a2a2lab/TamsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a2a2lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
